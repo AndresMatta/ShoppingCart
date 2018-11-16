2351c1e59016ca10aed6279d4b68dbf05220297a
Andres Matta Morales <andres.sm123@gmail.com>
2018-11-16 05:48:06 -0600

    Added "Basket" data access.

--- ShoppingCart/ShoppingCart.DataAccess.SQL/DataContext.cs
+++ ShoppingCart/ShoppingCart.DataAccess.SQL/DataContext.cs
@@ -17,5 +17,7 @@ namespace ShoppingCart.DataAccess.SQL
 
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductCategory> ProductCategories { get; set; }
+        public DbSet<Basket> Baskets { get; set; }
+        public DbSet<BasketItem> BasketItem { get; set; }
     }
 }
