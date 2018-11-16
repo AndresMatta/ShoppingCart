f6651ab286f839c71b81a0fe4c7498eaee1d19c5
Andres Matta Morales <andres.sm123@gmail.com>
2018-11-16 05:01:11 -0600

    Added product filtering.

---
+++ ShoppingCart/ShoppingCart.Core/ViewModels/ProductListViewModel.cs
@@ -0,0 +1,15 @@
+﻿using ShoppingCart.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.Core.ViewModels
+{
+    public class ProductListViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public IEnumerable<ProductCategory> ProductCategories { get; set; }
+    }
+}
